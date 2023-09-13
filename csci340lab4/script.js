9134c827804fef391a564352bf724a5fee413b12
elliotallen1 <allened@hendrix.edu>
2023-09-12 21:09:29 -0500

    Got quotes working
    Both buttons work and do as intended.

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
-    $('#image-button').click(function() {
+
+    $("#image-button").click(function() {
       $.ajax({
         dataType: "json",
         url: "https://random.dog/woof.json",
         success: function(results) {
           console.log(results["url"]);
           if (results["url"].endsWith(".mp4")) {
-            $('#dog-image').attr("src", "images.jpeg");
+            $("#dog-image").attr("src", "images.jpeg");
           } else {
-            $('#dog-image').attr("src", results["url"]).attr("width", 250).attr("height", 250);
+            $("#dog-image").attr("src", results["url"]).attr("width", 250).attr("height", 250);
           }
         },
         error: function(xhr,status,error) {
@@ -16,4 +17,21 @@ $(document).ready(function() {
         }
       });
     });
+
+    $('#quote-button').click(function() {
+        $.ajax({
+          dataType: "json",
+          url: "https://api.breakingbadquotes.xyz/v1/quotes",
+          success: function(results) {
+            console.log(results[0]["author"]);
+            $("#dog-name").html(results[0]["author"]);
+            $("#dog-quote").html(results[0]["quote"]);
+
+          },
+          error: function(xhr,status,error) {
+            console.log(error);
+          }
+        });
+      });
+      
   });
